4485ca19592dccf1c6226e94f7067015123bcc2c
James Calligeros <jcalligeros99@gmail.com>
2022-03-15 14:34:08 +1000

    install.py: fixed type issue
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- install.py
+++ install.py
@@ -14,6 +14,7 @@ def get_system():
     We only want the model number, which is the 5 characters after "apple,"
     '''
     with open("/sys/firmware/devicetree/base/compatible", "r") as sys
+        sys = sys.read()
         system_compatible = sys[6:10]
 
     return system_compatible
