ce4915b9d775f6ac5e8192cd7d8be35d639404e3
James Calligeros <jcalligeros99@gmail.com>
2022-03-16 17:01:53 +1000

    mac-audio.sh: fixes
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- mac-audio.sh
+++ mac-audio.sh
@@ -13,18 +13,27 @@ fi
 echo "We will now set up your Apple Silicon Mac's integrated audio."
 echo
 read -sp "Press Enter to continue..."
+echo
 
-if [[ -n /usr/share/pipewire/devices/apple/ ]] then;
+if [[ -d /usr/share/pipewire/devices/apple/ ]]; then
+    echo "Installation directory already exists."
+else
     echo "Install directory does not exist. Creating..."
     mkdir -p /usr/share/pipewire/devices/apple/
 fi
 
-exec python3 install.py
+python3 install.py
+
 echo "Restarting PipeWire..."
 killall pipewire
+
 echo
 echo "A new audio device should now be available to route audio."
 echo "For best results, make sure your DE selects it as the default"
 echo "device. Do not adjust alsamixer from here on out, all volume"
 echo "should be controlled via this new device."
+echo
+echo "Rebooting is highly encouraged and sometimes required to ensure"
+echo "all changes take effect."
+echo
 exit
