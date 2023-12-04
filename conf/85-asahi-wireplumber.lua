263dadabb4caf8b72b9d75013dc97a0b94998cf0
James Calligeros <jcalligeros99@gmail.com>
2023-12-04 20:56:39 +1000

    conf: remove load_pw_module() overload
    this is no longer necessary
    
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- conf/85-asahi-wireplumber.lua
+++ conf/85-asahi-wireplumber.lua
@@ -1,11 +1,3 @@
--- Override pw module loading func so we can pass args
-function load_pw_module(m, a)
-  assert(type(m) == "string", "module name is mandatory, bail out");
-  if not components[m] then
-    components[m] = { "libpipewire-module-" .. m, type = "pw_module", args = a }
-  end
-end
-
 -- Clamp util to keep Wireplumber on ecores
 mod_args = {
   ["nice.level"] = -11,
