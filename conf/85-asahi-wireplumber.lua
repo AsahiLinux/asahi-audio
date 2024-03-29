0174365c1a85e49b72ffde1a8ba2a3a300956bf1
James Calligeros <jcalligeros99@gmail.com>
2024-03-29 13:18:14 +1000

    conf: port sample rate limiter to WP 0.5 config fragment
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- conf/85-asahi-wireplumber.lua
+++ conf/85-asahi-wireplumber.lua
@@ -6,19 +6,5 @@
 -- }
 -- load_pw_module("rt", mod_args)
 
-ratelimit = {
-  matches = {
-    {
-      { "media.class", "matches", "*/Sink" },
-      { "node.name", "matches", "alsa_output.platform-sound.HiFi__hw_AppleJ*_1__sink" }
-    }
-  },
-  apply_properties = {
-    -- First entry is the fallback, hence 48k first
-    ["audio.allowed-rates"] = "48000,44100",
-  }
-}
-table.insert(alsa_monitor.rules, ratelimit)
-
 -- Load our policy to force the raw speaker volume to 100%
 load_script("policy-asahi.lua")
