96c0a026410cc57c846197d3327c48654e39c2ae
Hector Martin <marcan@marcan.st>
2024-11-29 18:31:06 +0900

    trim.py: Also trim head
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- tools/trim.py
+++ tools/trim.py
@@ -5,7 +5,8 @@ EPSILON = 1e-10 # -200dB
 rate, data = scipy.io.wavfile.read(sys.argv[1])
 
 count = len(numpy.trim_zeros((abs(data) > EPSILON), "b"))
-
 data = data[:count]
+count = len(numpy.trim_zeros((abs(data) > EPSILON), "f"))
+data = data[-count:]
 
 scipy.io.wavfile.write(sys.argv[2], rate, data)
